a4c7d7f5adc764dfd6ae1e4ba3ab004a88ec41c9
isaoeka <isaoeka@gmail.com>
2017-12-14 07:09:52 +0900

    Add ApiCall

--- TodayExtension/TodayViewController.swift
+++ TodayExtension/TodayViewController.swift
@@ -8,11 +8,27 @@
 
 import Cocoa
 import NotificationCenter
+import Alamofire
+import SwiftyJSON
 
 class TodayViewController: NSViewController, NCWidgetProviding {
+    static let qiitaToken: String = "XXXX"
 
     @IBOutlet weak private var scrollView: NSScrollView!
     @IBOutlet weak private var tableView: NSTableView!
+
+    struct QiitaItem {
+        let title: String
+        let url: String
+        let userId: String
+    }
+    private var items: [QiitaItem] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
 
     override var nibName: NSNib.Name? {
         return NSNib.Name("TodayViewController")
@@ -33,17 +49,47 @@ class TodayViewController: NSViewController, NCWidgetProviding {
     }
 
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        self.fetchData()
         // Update your data and prepare for a snapshot. Call completion handler when you are done
         // with NoData if nothing has changed or NewData if there is new data since the last
         // time we called you
         completionHandler(.noData)
+    }
+
+    private func fetchData() {
+        let url: String = "https://qiita.com/api/v2/items"
+        Alamofire.request(url,
+                          method: .get,
+                          parameters: [
+                            "page": 1,
+                            "pre_page": 20,
+                            "query": "tag:Swift"
+            ],
+                          headers: [
+                            "Authorization": " Bearer \(TodayViewController.qiitaToken)",
+            ]
+            ).responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    self.items = json.map { (_, json) in
+                        QiitaItem(
+                            title: json["title"].string ?? "",
+                            url: json["url"].string ?? "",
+                            userId: json["user"]["id"].string ?? ""
+                        )
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+        }
     }
 }
 
 extension TodayViewController: NSTableViewDelegate, NSTableViewDataSource {
 
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return 10
+        return self.items.count
     }
 
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -52,10 +98,19 @@ extension TodayViewController: NSTableViewDelegate, NSTableViewDataSource {
             else {
                 return nil
         }
-
-        cell.textField?.stringValue = "----- CELL ----"
+        let item = self.items[row]
+        cell.textField?.stringValue = item.title
         return cell
     }
+
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        let item = self.items[row]
+        print(item)
+
+        if let url = URL(string: item.url) {
+            NSWorkspace.shared.open(url)
+        }
+
+        return true
+    }
 }
-
-
