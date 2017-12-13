58b2c3798ecb97d451cb57e8d999c425fc30bd54
isaoeka <isaoeka@gmail.com>
2017-12-14 07:06:40 +0900

    Add NSTableView.

--- TodayExtension/TodayViewController.swift
+++ TodayExtension/TodayViewController.swift
@@ -11,8 +11,25 @@ import NotificationCenter
 
 class TodayViewController: NSViewController, NCWidgetProviding {
 
+    @IBOutlet weak private var scrollView: NSScrollView!
+    @IBOutlet weak private var tableView: NSTableView!
+
     override var nibName: NSNib.Name? {
         return NSNib.Name("TodayViewController")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.scrollView.drawsBackground = false
+
+        self.tableView.headerView = nil
+        self.tableView.backgroundColor = .clear
+        self.tableView.selectionHighlightStyle = .sourceList
+        self.tableView.gridColor = .lightGray
+        self.tableView.gridStyleMask = .solidHorizontalGridLineMask
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -21,5 +38,24 @@ class TodayViewController: NSViewController, NCWidgetProviding {
         // time we called you
         completionHandler(.noData)
     }
-
 }
+
+extension TodayViewController: NSTableViewDelegate, NSTableViewDataSource {
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let view = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cell"), owner: self),
+            let cell = view as? NSTableCellView
+            else {
+                return nil
+        }
+
+        cell.textField?.stringValue = "----- CELL ----"
+        return cell
+    }
+}
+
+
