bcb4388726ff3d10c11441ed7886e36a172963a6
isaoeka <isaoeka@gmail.com>
2017-12-14 06:52:55 +0900

    create project.

---
+++ WidgetSample-OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WidgetSample-OSX
+//
+//  Created by Isao Kono on 2017/12/14.
+//  Copyright © 2017年 isaoeka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WidgetSample-OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WidgetSample-OSX
+//
+//  Created by Isao Kono on 2017/12/14.
+//  Copyright © 2017年 isaoeka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
